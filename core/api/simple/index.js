b982f30e19fc1ddc4feacb80970c7b5e5415b54c
ghager93 <ghager93@gmail.com>
2023-05-04 21:46:41 +1000

    Model attached to api

--- core/api/simple/index.js
+++ core/api/simple/index.js
@@ -16,22 +16,19 @@ const createSimple = (req, res) => {
         req.on('end', () => {
             body = JSON.parse(Buffer.concat(chunks))
             console.log(body)
-        })
-        console.log(body)
+            const simple = new simpleModel(body);
+            console.log(simple)
 
-        const simple = new simpleModel(req.body);
+            res.statusCode = 200
+            res.setHeader('Content-Type', 'application/json')
+            res.end(JSON.stringify(simple))
+        })
     }
     catch(err) {
         res.statusCode = 404;
         res.setHeader('Content-Type', 'application/json')
         res.end(err.name + ": " + err.message);
-        return
     }
-
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'application/json')
-    res.end(simple)
-    return
 }
 
 export default handleSimple;
--- core/api/simple/model.js
+++ core/api/simple/model.js
@@ -1,25 +1,27 @@
+import { json } from "body-parser";
 import simpleSchema from "./schema";
 
 class simpleModel {
     constructor({name, number}) {
+        number = Number(number)
         if(name === undefined) {
             throw SyntaxError('Must include name.')
         }
         if(number === undefined) {
             throw SyntaxError('Must include number.')
         }
-        if(typeof(name) !== this.simpleSchema.name) {
-            throw TypeError(`Name must be ${this.simpleSchema.name}`)
+        if(typeof(name) !== simpleModel.schema.name) {
+            throw TypeError(`Name must be ${simpleModel.schema.name}`)
         }
-        if(typeof(number) !== this.simpleSchema.number) {
-            throw TypeError(`Number must be ${this.simpleSchema.number}`)
+        if(typeof(number) !== simpleModel.schema.number) {
+            throw TypeError(`Number must be ${simpleModel.schema.number}`)
         }
 
         this.name = name
         this.number = number
     }
 
-    static modelName = "simple"
+    static modelName = "simple";
     static schema = simpleSchema;
 }
 
